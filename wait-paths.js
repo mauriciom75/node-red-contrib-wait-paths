d2f33000137f4d1d6e1144642bd811dac4659198
mauriciom75 <mauriciomargot@gmail.com>
2017-09-30 01:04:51 -0300

    Update wait-paths.js

--- wait-paths.js
+++ wait-paths.js
@@ -3,42 +3,33 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
 
         this.paths = JSON.parse(config.paths);
-        //this.paths = config.paths;
 
         var node = this;
         node.on('input', function(msg) {
-            //msg.payload = msg.payload.toLowerCase();
-            //node.send(msg);
             
             var flow1 = this.context().flow;
-
             var msgid = msg._msgid.replace(".","0");
-            console.log(msgid);
-
             var paths = this.paths;
-            console.log(JSON.stringify(paths));
-
             var pathsLength = paths.length;
-
+            
+            // guardo variables que van llegando
             for (var i = 0; i < pathsLength; i++) {
-                console.log("pase por 1");
                 if (msg.paths[paths[i]]) {
-                    console.log("pase por 2");
                     flow1.set(msgid+paths[i],msg.paths[paths[i]]);
                 }
             }
 
+            // recupero variables para verificar si ya llegaron todas
             for (var i = 0; i < pathsLength; i++) {
                 msg.paths[paths[i]] = flow1.get(msgid+paths[i]);
-                console.log("pase por 3:" + msg.paths[paths[i]]);
             }
 
-            // evaluo si ya estan todos
+            // evaluo si ya llegaron todos
             for (var i = 0; i < pathsLength; i++) {
                 if ( !msg.paths[paths[i]] )
                     break;
             }
-            // si están todos devuelvo msg y elimino auxiliares.
+            // si llegaron todos devuelvo msg y elimino auxiliares.
             if ( i == pathsLength )
             {
                 for (var i = 0; i < pathsLength; i++) {
@@ -47,8 +38,6 @@ module.exports = function(RED) {
                 //return msg;
                 node.send(msg);
             }
-
-
             
             
         });
